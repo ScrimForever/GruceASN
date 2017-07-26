24f0c9fc80d26fa63be41aad494f7915430614f5
Thiago Salgado <scrimforever@gmail.com>
2017-07-26 10:50:52 -0300

    Editting Config

--- joinner.py
+++ joinner.py
@@ -9,16 +9,13 @@ from colorama import Fore
 import logmodule
 import connection
 import ConfigParser
+import sys
 
 init(autoreset=True)
 logmodule.start_log()
 
 
 class Joinner(object):
-
-    yesterday = (date.today() - timedelta(days=1))
-    yesterday = yesterday.strftime("%Y%m%d")
-    today = date.today().strftime("%Y%m%d")
 
     def open_config_ftp(self):
 
@@ -39,26 +36,35 @@ class Joinner(object):
             open_con = connection.FTP()
             ftp_con = open_con.open_connection_all(con_ftp)
             ftp_con.login()
-
             for i in range(0, len(conf_continents.items())):
-                print conf_continents.items()[i]
-                ftp_con.cwd(conf_continents.values()[i])
                 try:
-                    ftp_con.retrbinary('RETR delegated-%s-extended-%s.txt' % (conf_continents.keys()[i],Joinner.today), open('%s_%s.txt' % (conf_continents.keys()[i],Joinner.today), 'w'))
+                    print ("try")
+                    ftp_con.cwd(conf_continents.values()[i])
+                    print ftp_con.pwd()
+                    sys.stdout(ftp_con.dir("delegated-%s-extended-%s" % (conf_continents.keys()[i],)))
+                    f = open('%s_%s.txt' % (conf_continents.keys()[i],), 'w')
+                    ftp_con.retrbinary('RETR delegated-%s-extended-%s' % (conf_continents.keys()[i],), f.write)
 
                 except Exception as e:
-                    print e
-                break
+                    print ("except")
+                    ftp_con.cwd(conf_continents.values()[i])
+                    print ftp_con.pwd()
+                    f = open('%s_%s.txt' % (conf_continents.keys()[i], yesterday), 'wb')
+                    ftp_con.retrbinary('RETR delegated-%s-extended-%s' % (conf_continents.keys()[i], yesterday), f.write)
+
         except Exception as e:
             print e
+
+
     #def joiner_nic_all_contry(self,country):
 
     #def joiner_nic_country(self,continent,country):
 
 if __name__ == "__main__":
 
+
     x = Joinner()
-    print x.joiner_all(x.open_config_continents(), x.open_config_ftp())
+    x.joiner_all(x.open_config_continents(), x.open_config_ftp())
 
 
 
--- make_config.py
+++ make_config.py
@@ -1,17 +1,33 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import date,timedelta
 import ConfigParser
 
-parser = ConfigParser.RawConfigParser()
 
-parser.add_section('CONFIG')
-parser.set('CONFIG', 'FTP', 'ftp.lacnic.net')
-parser.add_section('CONTINENTS')
-parser.set("CONTINENTS", 'AFRINIC','/pub/stats/afrinic')
-parser.set("CONTINENTS", 'APINIC', '/pub/stats/apnic')
-parser.set("CONTINENTS", 'ARIN','/pub/stats/arin')
-parser.set("CONTINENTS", 'LACNIC','/pub/stats/lacnic')
-parser.set("CONTINENTS", 'RIPENCC','/pub/stats/ripencc')
-with open('config.ini', 'wb') as cf:
-    parser.write(cf)
+yesterday = (date.today() - timedelta(days=1))
+yesterday = yesterday.strftime("%Y%m%d")
+today = date.today().strftime("%Y%m%d")
+
+class RunConf(object):
+
+    def runConfig(self):
+        self.parser = ConfigParser.RawConfigParser()
+
+        self.parser.add_section('CONFIG')
+        self.parser.set('CONFIG', 'FTP', 'ftp.lacnic.net')
+        self.parser.add_section('CONTINENTS')
+        self.parser.set("CONTINENTS", 'AFRINIC','/pub/stats/afrinic')
+        self.parser.set("CONTINENTS", 'APINIC', '/pub/stats/apnic')
+        self.parser.set("CONTINENTS", 'ARIN','/pub/stats/arin')
+        self.parser.set("CONTINENTS", 'LACNIC','/pub/stats/lacnic')
+        self.parser.set("CONTINENTS", 'RIPENCC','/pub/stats/ripencc')
+        self.parser.add_section('DIR_LOC')
+        self.parser.set("DIR_LOC", 'afrinic', 'delegated-afrinic-extended-%s' % today)
+        self.parser.set("DIR_LOC", 'apnic', 'delegated-apnic-extended-%s' % today)
+        with open('config.ini', 'wb') as cf:
+            self.parser.write(cf)
+
 
 
 
